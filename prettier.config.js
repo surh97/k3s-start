255e799790c38c8c22c004d79c4cc0a4ee5db774
surh <surunhao@gmail.com>
2020-03-12 01:48:37 +0800

    init

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  eslintIntegration: true,
+  htmlWhitespaceSensitivity: 'css',
+  arrowParens: 'always',
+  jsxSingleQuote: true,
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'all',
+  printWidth: 80,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+};
